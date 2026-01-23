8b4090621f944abaf15d293181742d48c6372136
mohamedhassanmn <hassanaesthete008@gmail.com>
2026-01-23 15:36:09 +0530

    add notification page components

---
+++ app/components/atoms/tab-item.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <div class="px-2 cursor-pointer">
+        <div :class="{ 'bg-neutral-400 text-primary-100 px-2 py-1.5 rounded-3xl text-center': isActive, 'px-2': !isActive }">
+            {{ label }}
+        </div>
+    </div>
+</template>
--- app/pages/notifications.vue
+++ app/pages/notifications.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import Tab from '~/components/molecules/tab.vue'
+import NotificationsList from '~/components/organisms/notifications-list/index.vue';
+const tabs = ["General", "Submission", "Bookings"]
+const activeTab = ref(tabs[0] || '')
+</script>
+
 <template>
-    <div>
-        Welcome to the Notifications Page
-    </div>  
+    <div class="flex flex-col justify-center items-center">
+        <Tab :tabs="tabs" :activeTab="activeTab" @change-tab="activeTab = $event" />
+        <div class="mt-6 w-full">
+            <template v-if="activeTab === tabs[0]">
+                <NotificationsList />
+            </template>
+            <template v-else-if="activeTab === tabs[1]">
+                <p class="text-center text-neutral-600">Submission Notifications Content</p>
+            </template>
+            <template v-else-if="activeTab === tabs[2]">
+                <p class="text-center text-neutral-600">Bookings Notifications Content</p>
+            </template>
+        </div>
+    </div>
 </template>
