880353ad376d97586d6a1ba3feb377a7775e194a
mohamedhassanmn <hassanaesthete008@gmail.com>
2026-01-30 11:52:35 +0530

    create notifications page and add routes to communications and registrations page

--- app/pages/registration.vue
+++ app/pages/registration.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import Stepper from '~/components/molecules/stepper.vue'
+const activeStep = ref(1);
+const steps = [
+    { stepCount: 1, stepName: 'Person Info' },
+    { stepCount: 2, stepName: 'Registeration Info' },
+    { stepCount: 3, stepName: 'Payment' },
+];
+</script>
+
 <template>
-    <div>
-        Welcome to the Registration Page
-    </div>  
+    <Stepper :steps="steps" :activeStep="activeStep" @change-step="activeStep = $event" />
+    <div class="min-w-160 w-1/3 mx-auto">
+        <template v-if="activeStep == 1">
+            <div class="text-center mt-6">Person Info</div>
+        </template>
+        <template v-else-if="activeStep == 2">
+            <div class="text-center mt-6">Registration Info</div>
+        </template>
+        <template v-else-if="activeStep == 3">
+            <div class="text-center mt-6">Payment</div>
+        </template>
+    </div>
 </template>
