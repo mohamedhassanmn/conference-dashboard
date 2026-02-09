2848ba7f5042ec358538a5764000b062e221c358
mohamedhassan008.work <mohamedhassan008.work@gmail.com>
2026-02-10 02:38:01 +0530

    comment out api request

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
 
-console.log('Checking backend health...',config.public.apiBase)
-const { data, error } = await useFetch('http://backend:4500/')
+// console.log('Checking backend health...',config.public.apiBase)
+// const { data, error } = await useFetch('http://backend:4500/')
 
-console.log('data:', data.value)
-console.log('error:', error.value)
+// console.log('data:', data.value)
+// console.log('error:', error.value)
 </script>
 
 <template>
