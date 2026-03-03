4dcf4a810dd13ea7467747ca3499dfefbea7c663
mohamedhassan008.work <mohamedhassan008.work@gmail.com>
2026-03-03 17:00:33 +0000

    fix the layout sidebar same as the dashboard-landing layout

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -3,8 +3,8 @@ import Sidebar from '~/components/atoms/sidebar.vue';
 </script>
 
 <template>
-    <div class="flex h-screen">
-        <div class="w-[15%] h-full bg-primary-200">
+    <div class="flex h-screen overflow-hidden">
+        <div class="w-[18%] 4xl:w-[15%] min-w-[180px] h-full bg-primary-200 shrink-0">
             <Sidebar />
         </div>
         <div class="w-full h-full py-8 px-8 overflow-auto">
